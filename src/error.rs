4c11a8fd2c7993b92f824c56106796c6df5b6f94
Josh King <josh@sigmaprime.io>
2024-09-09 20:51:30 +1000

    adding config builder and error files

---
+++ src/error.rs
@@ -0,0 +1,18 @@
+/// Error associated with Config building.
+#[derive(Debug)]
+pub enum ConfigBuilderError {
+    /// Quorum size too small
+    QuorumSizeTooSmall,
+}
+
+impl std::error::Error for ConfigBuilderError {}
+
+impl std::fmt::Display for ConfigBuilderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::QuorumSizeTooSmall => {
+                write!(f, "Quorum size too small")
+            }
+        }
+    }
+}
