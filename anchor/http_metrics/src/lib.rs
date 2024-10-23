08105528385b119198dc91cb9a2f7c5071e826c4
Age Manning <Age@AgeManning.com>
2024-10-24 10:22:25 +1100

    Temp commit

---
+++ anchor/http_metrics/src/lib.rs
@@ -0,0 +1,7 @@
+//! Provides a metrics server for Anchor
+//!
+//! This may be a temporary addition, once the Lighthouse VC moves to axum we may be able to group
+//! code. a
+
+pub struct Metrics {}
+
