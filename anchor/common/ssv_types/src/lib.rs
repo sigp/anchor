0d9890bece24ee374cb771e00487d5db53402f95
Zacholme7 <zacholme@gmail.com>
2024-12-05 01:12:42 +0000

    cargo fmt

--- anchor/common/ssv_types/src/lib.rs
+++ anchor/common/ssv_types/src/lib.rs
@@ -1,6 +1,6 @@
 pub use committee::{CommitteeId, CommitteeMember};
 pub use operator::{Operator, OperatorId};
-pub use share::{SSVShare, ValidatorIndex, Share, ShareMember};
+pub use share::{SSVShare, Share, ShareMember, ValidatorIndex};
 mod committee;
 mod operator;
 mod share;
