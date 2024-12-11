bd0e451545d566faa3e33609856589905448fb29
Zacholme7 <zacholme@gmail.com>
2024-12-11 15:01:56 +0000

    move to byte array for encrypted private key

--- anchor/common/ssv_types/src/share.rs
+++ anchor/common/ssv_types/src/share.rs
@@ -1,10 +1,10 @@
-use types::{PublicKey, SecretKey};
+use types::PublicKey;
 
 /// One of N shares of a split validator key.
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct Share {
     /// The public key of this Share
     pub share_pubkey: PublicKey,
     /// The encrypted private key of the share
-    pub encrypted_private_key: SecretKey,
+    pub encrypted_private_key: [u8; 256],
 }
