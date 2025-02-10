e36ce2a3ab888711384a66011b1fe27d4b72d930
Zacholme7 <zacholme@gmail.com>
2025-02-10 22:08:45 +0000

    emit warm only when live

--- anchor/eth/src/event_processor.rs
+++ anchor/eth/src/event_processor.rs
@@ -82,9 +82,11 @@ impl EventProcessor {
                 .get(topic0)
                 .expect("Handler should always exist");
 
-            // Handle the log and emit warning for any malformed events
+            // Handle the log and emit a warning for the malformed event if we are in live sync
             if let Err(e) = handler(self, log) {
-                warn!("Malformed event: {e}");
+                if live {
+                    warn!("Malformed event: {e}");
+                }
                 continue;
             }
 
