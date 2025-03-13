9dbbc3e9326ebc811c2050e9d6be18cf44712b1c
ThreeHrSleep <threehrsleep@gmail.com>
2025-03-13 17:32:24 +0530

    suggestions

--- anchor/keygen/src/lib.rs
+++ anchor/keygen/src/lib.rs
@@ -1,9 +1,9 @@
 use base64::prelude::*;
 use clap::Parser;
-use openssl::rsa::Rsa;
+use openssl::{pkey::Private, rsa::Rsa};
 use serde::Serialize;
 use std::{fs, path::PathBuf};
-use tracing::info;
+use tracing::{info, warn};
 
 #[derive(Debug)]
 pub enum KeygenError {
@@ -27,7 +27,7 @@ struct PrettyOutput {
 }
 
 // Run RSA keygeneration
-pub fn run_keygen(keygen: Keygen) -> Result<(), KeygenError> {
+pub fn run_keygen(keygen: Keygen) -> Result<Rsa<Private>, KeygenError> {
     // Generate the new rsa private key
     let private_key = Rsa::generate(2048)
         .map_err(|e| KeygenError::Generate(format!("Failed to generate new private key: {e}")))?;
@@ -65,13 +65,6 @@ pub fn run_keygen(keygen: Keygen) -> Result<(), KeygenError> {
     let pem_file = output_dir.join("key.pem");
     let json_file = output_dir.join("keys.json");
 
-    // Write the PEM file
-    fs::write(&pem_file, &private_pem).map_err(|e| {
-        KeygenError::Output(format!("Failed to write private key to PEM file: {e}"))
-    })?;
-
-    info!("Private key written to: {}", pem_file.display());
-
     // Create JSON data structure
     let data = PrettyOutput {
         public: public_pem_encoded,
@@ -83,11 +76,25 @@ pub fn run_keygen(keygen: Keygen) -> Result<(), KeygenError> {
         KeygenError::Output(format!("Failed to convert output data to JSON string: {e}"))
     })?;
 
-    // Write the JSON file
-    fs::write(&json_file, pretty_json)
-        .map_err(|e| KeygenError::Output(format!("Failed to write keys to JSON file: {e}")))?;
+    if !pem_file.exists() && !json_file.exists() {
+        // Write the PEM file
+        fs::write(&pem_file, &private_pem).map_err(|e| {
+            KeygenError::Output(format!("Failed to write private key to PEM file: {e}"))
+        })?;
 
-    info!("JSON keys written to: {}", json_file.display());
+        info!("Private key written to: {}", pem_file.display());
 
-    Ok(())
+        // Write the JSON file
+        fs::write(&json_file, pretty_json)
+            .map_err(|e| KeygenError::Output(format!("Failed to write keys to JSON file: {e}")))?;
+
+        info!("JSON keys written to: {}", json_file.display());
+    } else {
+        warn!(
+            "PEM file or JSON file already exist in {:?}, skipping write",
+            output_dir
+        );
+    }
+
+    Ok(private_key)
 }
