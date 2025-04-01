8bd19d0d5d27c1a8dcfa9bb9f12f76e6e0bc569b
Zacholme7 <zacholme@gmail.com>
2025-04-01 12:51:14 +0000

    hide disable slashing protection command

--- anchor/client/src/cli.rs
+++ anchor/client/src/cli.rs
@@ -429,6 +429,7 @@ pub struct Node {
     #[clap(
         long,
         help = "Disable slashing protection for all validator clients. DO NOT ENABLE THIS UNLESS YOU HAVE A MORE THAN SUFFICIENT REASON TO",
+        hide = true,
         display_order = 0
     )]
     pub disable_slashing_protection: bool,
