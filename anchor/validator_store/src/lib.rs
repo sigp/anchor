fc9ec39514f4154f9f8614ee372b46e08031a248
Daniel Knopik <daniel@dknopik.de>
2025-04-03 17:24:41 +0200

    Fix used `PartialSignatureKind` for `Role::SyncCommittee`

--- anchor/validator_store/src/lib.rs
+++ anchor/validator_store/src/lib.rs
@@ -976,7 +976,7 @@ impl<T: SlotClock, E: EthSpec> ValidatorStore for AnchorValidatorStore<T, E> {
 
         let signature = self
             .collect_signature(
-                PartialSignatureKind::SelectionProofPartialSig,
+                PartialSignatureKind::ContributionProofs,
                 Role::SyncCommittee,
                 None,
                 self.validator(*validator_pubkey)?,
