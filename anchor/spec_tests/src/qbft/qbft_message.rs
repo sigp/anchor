0e7d22fe6622908a955c172d9a2c6c0b682ed5c3
Zacholme7 <zacholme@gmail.com>
2025-04-10 20:26:56 +0000

    rm test files

--- anchor/spec_tests/src/qbft/qbft_message.rs
+++ anchor/spec_tests/src/qbft/qbft_message.rs
@@ -1,8 +1,6 @@
-use crate::{SpecTest, SpecTestType, qbft::QbftSpecTestType};
+use serde::{Deserialize, Serialize};
 
-struct QbftMessageTest {
-    name: String,
-}
+use crate::{SpecTest, SpecTestType, qbft::QbftSpecTestType};
 
 impl SpecTest for QbftMessageTest {
     fn name(&self) -> &str {
@@ -18,4 +16,49 @@ impl SpecTest for QbftMessageTest {
     fn test_type() -> SpecTestType {
         SpecTestType::Qbft(QbftSpecTestType::QbftMessage)
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QbftMessageTest {
+    #[serde(rename = "Name")]
+    pub name: String,
+
+    #[serde(rename = "Messages")]
+    pub messages: Vec<InputMessage>,
+
+    #[serde(rename = "EncodedMessages")]
+    pub encoded_messages: Vec<String>,
+
+    #[serde(rename = "ExpectedRoots")]
+    pub expected_roots: Vec<Vec<u8>>,
+
+    #[serde(rename = "ExpectedError")]
+    pub expected_error: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InputMessage {
+    #[serde(rename = "Signatures")]
+    pub signatures: Vec<String>,
+
+    #[serde(rename = "OperatorIDs")]
+    pub operator_ids: Vec<u64>,
+
+    #[serde(rename = "SSVMessage")]
+    pub ssv_message: SsvMessage,
+
+    #[serde(rename = "FullData")]
+    pub full_data: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SsvMessage {
+    #[serde(rename = "MsgType")]
+    pub msg_type: u64,
+
+    #[serde(rename = "MsgID")]
+    pub msg_id: Vec<u8>,
+
+    #[serde(rename = "Data")]
+    pub data: String,
 }
