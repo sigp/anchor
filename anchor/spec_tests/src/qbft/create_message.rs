a1ed92b13a377e783564a8f45728a5a14be9dec3
Zacholme7 <zacholme@gmail.com>
2025-04-10 20:26:56 +0000

    rm test files

--- anchor/spec_tests/src/qbft/create_message.rs
+++ anchor/spec_tests/src/qbft/create_message.rs
@@ -1,9 +1,5 @@
 use crate::{QbftSpecTestType, SpecTest, SpecTestType};
 
-struct CreateMessageTest {
-    name: String,
-}
-
 impl SpecTest for CreateMessageTest {
     fn name(&self) -> &str {
         &self.name
@@ -18,4 +14,48 @@ impl SpecTest for CreateMessageTest {
     fn test_type() -> SpecTestType {
         SpecTestType::Qbft(QbftSpecTestType::CreateMessage)
     }
+}
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateMessageTest {
+    #[serde(rename = "Name")]
+    pub name: String,
+
+    #[serde(rename = "Value")]
+    pub value: Vec<u8>,
+
+    #[serde(rename = "StateValue")]
+    pub state_value: Option<String>,
+
+    #[serde(rename = "Round")]
+    pub round: u64,
+
+    #[serde(rename = "RoundChangeJustifications")]
+    pub round_change_justifications: Option<Vec<String>>,
+
+    #[serde(rename = "PrepareJustifications")]
+    pub prepare_justifications: Option<Vec<String>>,
+
+    #[serde(rename = "CreateType")]
+    pub create_type: CreateType,
+
+    #[serde(rename = "ExpectedRoot")]
+    pub expected_root: String,
+
+    #[serde(rename = "ExpectedError")]
+    pub expected_error: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum CreateType {
+    #[serde(rename = "CreatePrepare")]
+    Prepare,
+
+    #[serde(rename = "CreateCommit")]
+    Commit,
+
+    #[serde(rename = "CreateRoundChange")]
+    RoundChange,
 }
