66f0542e40982156984b9a9da02a769711862404
Daniel Knopik <daniel@dknopik.de>
2025-04-10 16:06:46 +0200

    fmt

--- anchor/keysplit/src/cli.rs
+++ anchor/keysplit/src/cli.rs
@@ -42,7 +42,12 @@ pub struct Onchain {
     #[clap(long, help = "RPC endpoint to access L1 data", value_name = "ENDPOINT")]
     pub rpc: String,
 
-    #[clap(long, help = "Mainnet, Holesky or Hoodi", value_name = "NETWORK", value_enum)]
+    #[clap(
+        long,
+        help = "Mainnet, Holesky or Hoodi",
+        value_name = "NETWORK",
+        value_enum
+    )]
     pub network: Network,
 }
 
