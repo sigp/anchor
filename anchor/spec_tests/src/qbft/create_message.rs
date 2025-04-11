55cec777c445732b1b065f33559da41ece87eb38
Zacholme7 <zacholme@gmail.com>
2025-04-11 17:12:25 +0000

    move struct defs over to qbft branch

--- anchor/spec_tests/src/qbft/create_message.rs
+++ anchor/spec_tests/src/qbft/create_message.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 use crate::{QbftSpecTestType, SpecTest, SpecTestType};
 
 impl SpecTest for CreateMessageTest {
@@ -16,46 +18,7 @@ impl SpecTest for CreateMessageTest {
     }
 }
 
-use serde::{Deserialize, Serialize};
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CreateMessageTest {
-    #[serde(rename = "Name")]
     pub name: String,
-
-    #[serde(rename = "Value")]
-    pub value: Vec<u8>,
-
-    #[serde(rename = "StateValue")]
-    pub state_value: Option<String>,
-
-    #[serde(rename = "Round")]
-    pub round: u64,
-
-    #[serde(rename = "RoundChangeJustifications")]
-    pub round_change_justifications: Option<Vec<String>>,
-
-    #[serde(rename = "PrepareJustifications")]
-    pub prepare_justifications: Option<Vec<String>>,
-
-    #[serde(rename = "CreateType")]
-    pub create_type: CreateType,
-
-    #[serde(rename = "ExpectedRoot")]
-    pub expected_root: String,
-
-    #[serde(rename = "ExpectedError")]
-    pub expected_error: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum CreateType {
-    #[serde(rename = "CreatePrepare")]
-    Prepare,
-
-    #[serde(rename = "CreateCommit")]
-    Commit,
-
-    #[serde(rename = "CreateRoundChange")]
-    RoundChange,
 }
--- anchor/spec_tests/src/qbft/round_robin.rs
+++ anchor/spec_tests/src/qbft/round_robin.rs
@@ -19,49 +19,6 @@ impl SpecTest for RoundRobinTest {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct RoundRobinTest {
-    #[serde(rename = "Name")]
-    pub name: String,
-
-    #[serde(rename = "Share")]
-    pub share: CommitteeMember,
-
-    #[serde(rename = "Heights")]
-    pub heights: Vec<u64>,
-
-    #[serde(rename = "Rounds")]
-    pub rounds: Vec<u64>,
-
-    #[serde(rename = "Proposers")]
-    pub proposers: Vec<u64>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CommitteeMember {
-    #[serde(rename = "OperatorID")]
-    pub operator_id: u64,
-
-    #[serde(rename = "CommitteeID")]
-    pub committee_id: Vec<u8>,
-
-    #[serde(rename = "SSVOperatorPubKey")]
-    pub ssv_operator_pub_key: String,
-
-    #[serde(rename = "FaultyNodes")]
-    pub faulty_nodes: u64,
-
-    #[serde(rename = "Committee")]
-    pub committee: Vec<CommitteeMemberInfo>,
-
-    #[serde(rename = "DomainType")]
-    pub domain_type: Vec<u8>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CommitteeMemberInfo {
-    #[serde(rename = "OperatorID")]
-    pub operator_id: u64,
-
-    #[serde(rename = "SSVOperatorPubKey")]
-    pub ssv_operator_pub_key: String,
+struct RoundRobinTest {
+    name: String,
 }
