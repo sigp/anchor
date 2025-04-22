dd89326da0d139f3927a59d40e329aa3174de12b
Daniel Knopik <daniel@dknopik.de>
2025-04-22 14:14:39 +0200

    explain unsafe

--- anchor/src/main.rs
+++ anchor/src/main.rs
@@ -29,7 +29,9 @@ pub enum AnchorSubcommands {
 fn main() {
     // Enable backtraces unless a RUST_BACKTRACE value has already been explicitly provided.
     if std::env::var("RUST_BACKTRACE").is_err() {
-        // TODO: Audit that the environment access only happens in single-threaded code.
+        // `set_var` is marked unsafe because it is unsafe to use if there are multiple threads
+        // reading or writing from the environment. We are at the very beginning of execution and 
+        // have not spun up any threads or the tokio runtime, so it is safe to use.
         unsafe { std::env::set_var("RUST_BACKTRACE", "1") };
     }
 
