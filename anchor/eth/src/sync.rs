88be20ebd6610a71d1f074f76de14849b475d6a9
Daniel Knopik <daniel@dknopik.de>
2025-05-14 13:53:45 +0200

    format

--- anchor/eth/src/sync.rs
+++ anchor/eth/src/sync.rs
@@ -628,7 +628,7 @@ impl SsvEventSyncer {
                 // Block we are interested in is the current block number - follow distance
                 let relevant_block = block_header.number - FOLLOW_DISTANCE;
 
-                // If the relevant block was already processed, do not process it again. This can 
+                // If the relevant block was already processed, do not process it again. This can
                 // happen if `block_header.number` was seen before due to a reorg.
                 if relevant_block <= self.event_processor.db.state().get_last_processed_block() {
                     debug!(
