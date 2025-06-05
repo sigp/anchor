b037292652f508dffcf1a99c01831fe26114f7d0
Daniel Knopik <daniel@dknopik.de>
2025-06-05 14:08:10 +0200

    Avoid accidentally skipping blocks when switching to live sync

--- anchor/eth/src/sync.rs
+++ anchor/eth/src/sync.rs
@@ -641,7 +641,8 @@ impl SsvEventSyncer {
 
                 // If the relevant block was already processed, do not process it again. This can
                 // happen if `block_header.number` was seen before due to a reorg.
-                if relevant_block <= self.event_processor.db.state().get_last_processed_block() {
+                let last_processed = self.event_processor.db.state().get_last_processed_block();
+                if relevant_block <= last_processed {
                     debug!(
                         block_number = block_header.number,
                         relevant_block, "Already synced block - likely reorg"
@@ -661,7 +662,7 @@ impl SsvEventSyncer {
 
                 let mut logs = self
                     .fetch_logs(
-                        relevant_block,
+                        last_processed + 1,
                         relevant_block,
                         contract_address,
                         SSV_EVENTS.clone(),
