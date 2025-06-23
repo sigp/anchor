19e9a2be9dc7318b3b2451b7669f088a9aedcee7
Daniel Knopik <daniel@dknopik.de>
2025-06-23 16:45:48 +0200

    Call `public_key_from_pem_pkcs1` instead of converting to pkcs8

--- anchor/common/ssv_types/src/util.rs
+++ anchor/common/ssv_types/src/util.rs
@@ -8,20 +8,8 @@ pub fn parse_rsa(pem_data: &[u8]) -> Result<Rsa<Public>, String> {
         .decode(pem_data)
         .map_err(|e| format!("Unable to decode base64 pem data: {e}"))?;
 
-    // Convert the decoded data to a string
-    let mut pem_string = String::from_utf8(pem_decoded)
-        .map_err(|e| format!("Unable to convert decoded pem data into a string: {e}"))?;
-
-    // Fix the header - replace PKCS1 header with PKCS8 header
-    pem_string = pem_string
-        .replace(
-            "-----BEGIN RSA PUBLIC KEY-----",
-            "-----BEGIN PUBLIC KEY-----",
-        )
-        .replace("-----END RSA PUBLIC KEY-----", "-----END PUBLIC KEY-----");
-
     // Parse the PEM string into an RSA public key using PKCS8 format
-    let rsa_pubkey = Rsa::public_key_from_pem(pem_string.as_bytes())
+    let rsa_pubkey = Rsa::public_key_from_pem_pkcs1(&pem_decoded)
         .map_err(|e| format!("Failed to parse RSA public key: {e}"))?;
 
     Ok(rsa_pubkey)
