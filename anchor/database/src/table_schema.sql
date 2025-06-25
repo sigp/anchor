de5bf71b16f44514a3c0e6171fb404463ed689ab
petarjuki7 <petar.jukic7@gmail.com>
2025-06-26 00:58:48 +0200

    Combine owners and nonce tables

--- anchor/database/src/table_schema.sql
+++ anchor/database/src/table_schema.sql
@@ -5,11 +5,7 @@ INSERT INTO block (block_number) VALUES (0);
 
 CREATE TABLE owners (
     owner TEXT PRIMARY KEY,
-    fee_recipient TEXT NOT NULL
-);
-
-CREATE TABLE nonce (
-    owner TEXT NOT NULL PRIMARY KEY,
+    fee_recipient TEXT,
     nonce INTEGER DEFAULT 0
 );
 
