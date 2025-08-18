387711d138a725c42f74f719741011fa87c37dd7
Daniel Knopik <daniel@dknopik.de>
2025-08-18 11:58:58 +0200

    clone the is_synced

--- anchor/client/src/lib.rs
+++ anchor/client/src/lib.rs
@@ -470,7 +470,7 @@ impl Client {
             qbft_manager.clone(),
             signature_collector.clone(),
             database.watch(),
-            is_synced,
+            is_synced.clone(),
             outcome_tx,
             message_validator,
         );
