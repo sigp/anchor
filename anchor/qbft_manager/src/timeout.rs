af6ecf02aadfee7af43f64dbc267c61de53693f1
petarjuki7 <petar.jukic7@gmail.com>
2025-09-18 10:38:54 +0200

    check everything

--- anchor/qbft_manager/src/timeout.rs
+++ anchor/qbft_manager/src/timeout.rs
@@ -10,7 +10,7 @@ pub fn calculate_round_timeout(round: u64, start_time: &Instant) -> Option<Insta
     let additional_timeout = if round <= QUICK_TIMEOUT_THRESHOLD {
         // If we are below the quick timeout threshold the additonal timeout is round *
         // QUICK_TIMEOUT
-        Duration::from_secs(round * QUICK_TIMEOUT)
+        Duration::from_secs(round.checked_mul(QUICK_TIMEOUT)?)
     } else {
         // For higher rounds, use a combination of quick and slow timeouts
 
@@ -18,7 +18,9 @@ pub fn calculate_round_timeout(round: u64, start_time: &Instant) -> Option<Insta
         let quick_portion = Duration::from_secs(QUICK_TIMEOUT_THRESHOLD * QUICK_TIMEOUT);
 
         // The slow poritin is (round - threshold) * SLOW_TIMEOUT
-        let slow_portion = Duration::from_secs((round - QUICK_TIMEOUT_THRESHOLD) * SLOW_TIMEOUT);
+        let slow_portion = Duration::from_secs(
+            (round.checked_sub(QUICK_TIMEOUT_THRESHOLD))?.checked_mul(SLOW_TIMEOUT)?,
+        );
 
         quick_portion.checked_add(slow_portion)?
     };
