ac460188e5f7a78d4288c31e9a46c4a0575f0096
Daniel Knopik <daniel@dknopik.de>
2025-09-19 11:55:59 +0200

    feat: Add logging flags for dependencies

--- anchor/logging/src/logging.rs
+++ anchor/logging/src/logging.rs
@@ -59,6 +59,23 @@ pub struct FileLoggingFlags {
 
     #[arg(long, global = true, help = "Enables colors in logfile.")]
     pub logfile_color: bool,
+
+    #[arg(
+        long,
+        global = true,
+        default_value_t = Level::WARN,
+        value_parser = Level::from_str,
+        help = "Specifies the verbosity level used for the discv5 dependency log file")]
+    pub discv5_debug_level: Level,
+
+    #[arg(
+        long,
+        global = true,
+        default_value_t = Level::WARN,
+        value_parser = Level::from_str,
+        help = "Specifies the verbosity level used for the libp2p dependency log file. \
+                Certain score penalty information is logged regardless of this setting.")]
+    pub libp2p_debug_level: Level,
 }
 
 impl FileLoggingFlags {
