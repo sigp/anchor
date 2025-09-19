b7dc69ce6979bcf3d615231c5527e88b3bb911a1
Daniel Knopik <daniel@dknopik.de>
2025-09-19 09:47:01 +0200

    fix merge

--- anchor/network/src/discovery.rs
+++ anchor/network/src/discovery.rs
@@ -180,15 +180,16 @@ impl Discovery {
         );
 
         // discv5 configuration
-        let discv5_config_builder =
-            discv5::ConfigBuilder::new(discv5_listen_config).protocol_identity(protocol_identity);
+        let mut discv5_config_builder = discv5::ConfigBuilder::new(discv5_listen_config);
 
         // Apply discovery options
         if network_config.disable_enr_auto_update {
             discv5_config_builder.disable_enr_update();
         }
 
-        let discv5_config = discv5_config_builder.build();
+        let discv5_config = discv5_config_builder
+            .protocol_identity(protocol_identity)
+            .build();
 
         // convert the keypair into an ENR key
         let enr_key: CombinedKey =
