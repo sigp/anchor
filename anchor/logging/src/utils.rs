d70adf8799012a30c09084b741fa5c4016221aa0
Age Manning <Age@AgeManning.com>
2025-10-07 16:22:15 +1100

    Improve metric logging counts to be more useful

--- anchor/logging/src/utils.rs
+++ anchor/logging/src/utils.rs
@@ -3,8 +3,8 @@ use std::collections::HashSet;
 use tracing_subscriber::filter::FilterFn;
 use workspace_members::workspace_crates;
 
-const WORKSPACE_CRATES: &[&str] = workspace_crates!();
-const LIGHTHOUSE_CRATES: &[&str] = &[
+pub(crate) const WORKSPACE_CRATES: &[&str] = workspace_crates!();
+pub(crate) const LIGHTHOUSE_CRATES: &[&str] = &[
     "beacon_node_fallback",
     "slashing_protection",
     "task_executor",
@@ -12,8 +12,8 @@ const LIGHTHOUSE_CRATES: &[&str] = &[
 ];
 
 /// Constructs a filter which only permits logging from crates which are members of the workspace.
-pub fn build_workspace_filter()
--> Result<FilterFn<impl Fn(&tracing::Metadata) -> bool + Clone>, String> {
+pub fn build_workspace_filter(
+) -> Result<FilterFn<impl Fn(&tracing::Metadata) -> bool + Clone>, String> {
     let mut workspace_crates: HashSet<&str> = WORKSPACE_CRATES.iter().copied().collect();
     workspace_crates.extend(LIGHTHOUSE_CRATES.iter().copied());
 
