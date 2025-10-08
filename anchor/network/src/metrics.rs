f9db484870ac0300899a5a1316841c1471d13065
Josh King <josh@sigmaprime.io>
2025-10-08 15:39:01 +1100

    rebase on v1.0.0

--- anchor/network/src/metrics.rs
+++ anchor/network/src/metrics.rs
@@ -5,43 +5,6 @@ use metrics::*;
 pub static PEERS_CONNECTED: LazyLock<Result<IntGauge>> = LazyLock::new(|| {
     try_create_int_gauge("libp2p_peers", "Count of libp2p peers currently connected")
 });
-<<<<<<< HEAD
-=======
-
-pub static PEERS_BLOCKED_LOW_SCORE: LazyLock<Result<IntGauge>> = LazyLock::new(|| {
-    try_create_int_gauge(
-        "libp2p_blocked_peers_low_score",
-        "Count of peers blocked due to low gossipsub score",
-    )
-});
-
-pub static PEERS_BLOCKED_FAILED_HANDSHAKE: LazyLock<Result<IntGauge>> = LazyLock::new(|| {
-    try_create_int_gauge(
-        "libp2p_blocked_peers_failed_handshake",
-        "Count of peers blocked due to failed handshake",
-    )
-});
-
-pub static PEERS_BLOCKED_OUTGOING_CONNECTION_ERROR: LazyLock<Result<IntGauge>> = LazyLock::new(|| {
-    try_create_int_gauge(
-        "libp2p_blocked_peers_outgoing_connection_error",
-        "Count of peers blocked due to outgoing connection errors",
-    )
-});
-
-pub static PEERS_BLOCKED_INCOMING_CONNECTION_ERROR: LazyLock<Result<IntGauge>> = LazyLock::new(|| {
-    try_create_int_gauge(
-        "libp2p_blocked_peers_incoming_connection_error",
-        "Count of peers blocked due to incoming connection errors",
-    )
-});
-
-pub static PEERS_BLOCKED_OTHER: LazyLock<Result<IntGauge>> = LazyLock::new(|| {
-    try_create_int_gauge(
-        "libp2p_blocked_peers_other",
-        "Count of peers blocked for other reasons",
-    )
-});
 
 pub static HANDSHAKE_SUCCESSFUL: LazyLock<Result<IntCounter>> = LazyLock::new(|| {
     try_create_int_counter(
@@ -64,4 +27,4 @@ pub static HANDSHAKE_SUBNET_MATCHES: LazyLock<Result<IntGaugeVec>> = LazyLock::n
         &["match_count"],
     )
 });
->>>>>>> a40904f9 (feat: calculate subnets for handshake)
+
