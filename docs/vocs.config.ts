c7d0f401b903f4d19ad4b03e47474146bff55404
Tan Chee Keong <tanck@sigmaprime.io>
2025-11-11 11:24:30 +0800

    update book to include mainnet

--- docs/vocs.config.ts
+++ docs/vocs.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     { text: 'Documentation', link: '/introduction' },
     { text: 'GitHub', link: 'https://github.com/sigp/anchor' },
     {
-      text: 'v1.0.0-rc.1',
+      text: 'v1.0.0',
       items: [
         {
           text: 'Releases',
