05e579bebf56ef443228370b4b3787d94557ccb8
Daniel Knopik <daniel@dknopik.de>
2025-12-10 14:10:28 +0100

    fix incorrect display copy/paste

--- anchor/common/ssv_types/src/consensus.rs
+++ anchor/common/ssv_types/src/consensus.rs
@@ -805,7 +805,7 @@ impl Display for CheckpointMismatch {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Epoch mismatch: SOURCE: our {:?}, proposed {:?}. TARGET: our {:?}, proposed {:?}",
+            "Checkpoint mismatch: SOURCE: our {:?}, proposed {:?}. TARGET: our {:?}, proposed {:?}",
             self.our_source, self.proposed_source, self.our_target, self.proposed_target
         )
     }
