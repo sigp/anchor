7a41a9ae53b658a93ef9209f0a7731cbf1b4da91
petarjuki7 <petar.jukic7@gmail.com>
2025-12-19 15:44:42 +0100

    linter fix

--- anchor/eth/src/event_processor.rs
+++ anchor/eth/src/event_processor.rs
@@ -212,9 +212,7 @@ impl EventProcessor {
             )));
         }
 
-        self.db
-            .set_max_operator_id_seen(operatorId, tx)
-            .map_err(|e| ExecutionError::Database(e))?;
+        self.db.set_max_operator_id_seen(operatorId, tx)?;
 
         let data = publicKey.as_ref();
 
