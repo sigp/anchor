daec148ded03107a48828887bae926e23d57c846
shane-moore <skm1790@gmail.com>
2026-01-20 17:57:51 -0800

    feat: agg committee pre consensus p2p and qbft validation

--- anchor/message_receiver/src/manager.rs
+++ anchor/message_receiver/src/manager.rs
@@ -14,6 +14,7 @@ use slot_clock::SlotClock;
 use ssv_types::msgid::DutyExecutor;
 use tokio::sync::{mpsc, mpsc::error::TrySendError, watch};
 use tracing::{debug, debug_span, error, trace};
+use types;
 
 use crate::MessageReceiver;
 
