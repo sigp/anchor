db01b2ca287e1db5d43fd1d60fc19a15bbc2bc11
Daniel Knopik <daniel@dknopik.de>
2026-01-23 11:58:12 +0100

    fmt

--- anchor/common/qbft/src/tests.rs
+++ anchor/common/qbft/src/tests.rs
@@ -442,9 +442,10 @@ fn test_leader_waits_when_highest_prepared_data_missing() {
             identifier: VariableList::repeat_full(0),
             root: prepared_hash, // Claims this hash was prepared
             data_round: 1,       // Claims preparation happened in round 1
-            round_change_justification: VariableList::empty(), /* No RC justifications needed for this test */
-            prepare_justification: VariableList::empty(),      /* Should have prepare messages but
-                                                                * we'll skip validation */
+            round_change_justification: VariableList::empty(), /* No RC justifications needed for
+                                  * this test */
+            prepare_justification: VariableList::empty(), /* Should have prepare messages but
+                                                           * we'll skip validation */
         };
 
         let ssv_message = SSVMessage::new(
