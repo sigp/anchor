608f1494b3180d6cbf0dd2cabc07e54b315581f7
jnhsigmap <jason.harris@sigmaprime.io>
2026-03-19 15:16:53 +1100

    explain build.rs

--- anchor/cli/build.rs
+++ anchor/cli/build.rs
@@ -1,4 +1,5 @@
 fn main() {
     // This build script exists solely to ensure that `OUT_DIR` is set at compile time,
     // which is used by `build_profile_name()` to determine the build profile.
+    // See https://stackoverflow.com/questions/73595435/how-to-get-profile-from-cargo-toml-in-build-rs-or-at-runtime
 }
