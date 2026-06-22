5f58ee5084bba83c2b790e98b967f81ce0cbf430
Daniel Knopik <daniel@dknopik.de>
2026-06-22 13:48:29 +0200

    chore: Release v1.2.4

--- anchor/common/version/src/lib.rs
+++ anchor/common/version/src/lib.rs
@@ -17,8 +17,8 @@ pub const VERSION: &str = git_version!(
         // NOTE: using --match instead of --exclude for compatibility with old Git
         "--match=thiswillnevermatchlol"
     ],
-    prefix = "Anchor/v1.2.3-",
-    fallback = "Anchor/v1.2.3"
+    prefix = "Anchor/v1.2.4-",
+    fallback = "Anchor/v1.2.4"
 );
 
 /// Returns the first eight characters of the latest commit hash for this build.
